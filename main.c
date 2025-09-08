6a23164ef17cca6aa04f55173404ef363b1e6081
Meera Shersheer <meera04qasemshersheer@gmail.com>
2025-09-08 19:15:40 +0300

    wrote the flow for checking the link for the file (before parsing stage)

---
+++ main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshershe <mshershe@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 19:09:56 by mshershe          #+#    #+#             */
+/*   Updated: 2025/09/08 19:14:45 by mshershe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3D.h"
+
+int main(int argc, char *argv[])
+{
+	//first tke the link of the map file
+	//reprint it on terminal as a first check
+	//write function checks
+	//check extension
+	//check if file exist 
+	// check if the file has the right permissions
+	// the file is working --> great 
+	//now for parsing
+	// open map and read it using read 
+	//and do the checks
+	
+}
