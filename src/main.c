a8919a358d2edfaabde211587c3d47aeb426f423
Meera Shersheer <meera04qasemshersheer@gmail.com>
2025-09-09 17:24:42 +0300

    fixed error handling flow

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mshershe <mshershe@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 19:09:56 by mshershe          #+#    #+#             */
-/*   Updated: 2025/09/09 16:42:37 by mshershe         ###   ########.fr       */
+/*   Updated: 2025/09/09 17:24:04 by mshershe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int main(int argc, char *argv[])
 		printf ("Error\nIncorrect number of arguments\n");
 		return (1);
 	}
-	check_arg(argv[1]);
+	if (check_arg(argv[1]))
+		return (1);
 	if (check_map(argv, &map))
 		return (1);
 	
