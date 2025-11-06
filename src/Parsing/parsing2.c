516b3d8f90eff3808e6f96d5bb81a93a220e2ab1
Meera Shersheer <meera04qasemshersheer@gmail.com>
2025-11-06 20:07:21 +0300

    fixing coneccted defintion of textures and paths

--- src/Parsing/parsing2.c
+++ src/Parsing/parsing2.c
@@ -6,7 +6,7 @@
 /*   By: mshershe <mshershe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 21:17:15 by aalmahas          #+#    #+#             */
-/*   Updated: 2025/11/06 19:40:21 by mshershe         ###   ########.fr       */
+/*   Updated: 2025/11/06 20:06:26 by mshershe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	set_texture(char **texture_field, const char *line, t_map *map,
 	while (len > 0 && (line[len - 1] == ' ' || line[len - 1] == '\n' || line[len - 1] == '\t'))
 		len--;
 	if (len == 0)
-		error_exit(map, err_msg);
+		error_exit(map, "Texture is not defined");
 	*texture_field = ft_substr(line, 0, len);
 	if (!*texture_field)
 		error_exit(map, "malloc failed");
@@ -36,12 +36,16 @@ void	handle_ns_textures(char *line, t_map *map)
 	if (line[0] == 'N' && line[1] == 'O')
 	{
 		i = skip_spaces(line, 2);
+		if (i == 2)
+			error_exit(map, "Invalid Texture Definition Format");
 		set_texture(&map->north_texture, line + i, map,
 			"North texture defined more than once");
 	}
 	else if (line[0] == 'S' && line[1] == 'O')
 	{
 		i = skip_spaces(line, 2);
+		if (i == 2)
+			error_exit(map, "Invalid Texture Definition Format");
 		set_texture(&map->south_texture, line + i, map,
 			"South texture defined more than once");
 	}
@@ -54,12 +58,16 @@ void	handle_we_textures(char *line, t_map *map)
 	if (line[0] == 'W' && line[1] == 'E')
 	{
 		i = skip_spaces(line, 2);
+		if (i == 2)
+			error_exit(map, "Invalid Texture Definition Format");
 		set_texture(&map->west_texture, line + i, map,
 			"West texture defined more than once");
 	}
 	else if (line[0] == 'E' && line[1] == 'A')
 	{
 		i = skip_spaces(line, 2);
+		if (i == 2)
+			error_exit(map, "Invalid Texture Definition Format");
 		set_texture(&map->east_texture, line + i, map,
 			"East texture defined more than once");
 	}
